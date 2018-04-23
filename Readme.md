811e8dcbd7a99675891bdd2a76569d65ed8062e0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to add a text box field to a document
+
+
+This example shows how to create a text box field and add it to a PDF document.
+
+<br/>
+
+
---
+++ VB/AddTextBoxField/Program.vb
@@ -0,0 +1,37 @@
+﻿Imports DevExpress.Pdf
+Imports System.Drawing
+
+Namespace AddTextBoxField
+    Friend Class Program
+        Shared Sub Main(ByVal args() As String)
+            Using processor As New PdfDocumentProcessor()
+
+                ' Create an empty document. 
+                processor.CreateEmptyDocument("..\..\Result.pdf")
+
+                ' Create and draw a text box field.
+                Using graphics As PdfGraphics = processor.CreateGraphics()
+                    DrawTextBoxField(graphics)
+
+                    ' Render a page with graphics.
+                    processor.RenderNewPage(PdfPaperSize.Letter, graphics)
+                End Using
+            End Using
+        End Sub
+
+        Private Shared Sub DrawTextBoxField(ByVal graphics As PdfGraphics)
+
+            ' Create a text box field and specify its location on the page using a RectangleF object.
+            Dim textBox As New PdfGraphicsAcroFormTextBoxField("text box", New RectangleF(0, 10, 200, 30))
+
+            ' Specify text box properties.
+            textBox.Text = "Text Box"
+            textBox.TextAlignment = PdfAcroFormStringAlignment.Near
+            textBox.Appearance.FontSize = 12
+            textBox.Appearance.BackgroundColor = Color.AliceBlue
+
+            ' Add the field to graphics.
+            graphics.AddFormField(textBox)
+        End Sub
+    End Class
+End Namespace
